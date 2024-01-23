291a652041ae704dfbc4f9ff85f2e9aa9da482ec
technoyap <83795023+r4ty34r@users.noreply.github.com>
2024-01-22 21:35:17 -0500

    Add files via upload

---
+++ Assignment.h
@@ -0,0 +1,33 @@
+#pragma once // guardrail
+#include <iostream>
+
+// base class interface
+// 	private: currentSpeed, currentDirection, currentLocation
+// 	public: accel(toSpeed), decel(toSpeed), turn(direction, degrees) 
+//
+// 	car: inherits from base class no modification
+// 	truck: inherites from car
+// 		private: loadWeight, public: additional method to SetLoadWeight(weight) 
+
+
+// base class
+class Vehicle {
+	public: 
+		Vehicle(); // default consturctor
+		void accelerate(float toSpeed);
+		void decelerate(float toSpeed);
+		void turn(std::string toDirection, float degrees);	
+	private:
+		float currentSpeed;
+		std::string currentDirection;
+		std::string currentLocation;
+}; //end base class
+
+class Car : public Vehicle{};
+
+class Truck : public Car {
+	public: 
+		void setLoadWeight(float weightToLoad);
+	private:
+		float loadWeight; // total weight carried by truck
+}; 
